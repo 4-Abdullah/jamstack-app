385abb815612834136661b4c1c0e32c727b3a7b5
4-Abdullah <abdullahmaliksher@gmail.com>
2024-06-23 22:46:02 +0500

    type change

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,7 +4,7 @@ import Productdetails from './productdetails'
 import Data from './data'
 import Param from './param'
 import { Suspense } from 'react'
-const Slug =async ({searchparams,}:{searchparams:{slug:string|undefined;username:string|undefined}}) => {
+const Slug =async ({searchparams}:{searchparams:{slug:any;username:any}}) => {
  
   console.log(searchparams)
  
