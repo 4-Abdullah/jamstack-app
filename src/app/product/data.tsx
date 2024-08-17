36f1bb76d75bcbb27906c35dc93142f84e936bd8
4-Abdullah <abdullahmaliksher@gmail.com>
2024-08-17 11:00:27 +0500

    file type changed from tsx to jsx

--- src/app/product/data.tsx
+++ src/app/product/data.tsx
@@ -6,23 +6,23 @@ const data=async({
 }: {
   slug: string | undefined;user: string | undefined 
 })=> {
-const increaseCartQuantity=async(e)=>{
-    const data1 = await axios.get(`http://127.0.0.1:1337/api/carts?populate=*&filters[$and][0][user][username][$eq]=${user}`);
-    console.log(data1)
-    const getid=data1.data.id
-    // const res = await axios.get("http://localhost:1337/api/carts")
-    // console.log(res)
-    // const max=res.meta.pagination.total
-    console.log(getid)
-    const data = await axios.put(`http://localhost:1337/api/carts/${getid}`,{
-      "data":{
-        "products":{"connect":[e]}
-     }
-    })
+// const increaseCartQuantity=async(e)=>{
+//     const data1 = await axios.get(`http://127.0.0.1:1337/api/carts?populate=*&filters[$and][0][user][username][$eq]=${user}`);
+//     console.log(data1)
+//     const getid=data1.data.id
+//     // const res = await axios.get("http://localhost:1337/api/carts")
+//     // console.log(res)
+//     // const max=res.meta.pagination.total
+//     console.log(getid)
+//     const data = await axios.put(`http://localhost:1337/api/carts/${getid}`,{
+//       "data":{
+//         "products":{"connect":[e]}
+//      }
+//     })
     
-  }
+//   }
 const data = await fetch('http://127.0.0.1:1337/api/products?filters[slug]='+slug+'&populate=*');
-  console.log(data?.data)
+  console.log(data)
   const products = await data.json()
   console.log(products)
   return(
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,7 @@
 
 import { Suspense } from 'react';
 import Navbar from './index2'
-const Header =async ({searchparams,}:{searchparams:{slug:string|undefined ; username:string|undefined}}) => {
-  
-  console.log(searchparams)
+const Header =async () => {
   
   return(
         <>
